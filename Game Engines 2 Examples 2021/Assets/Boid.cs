b5a63d4ed733bcca4d5ea5f3c588fabcd3a8630b
Bryan Duggan <skooter500@gmail.com>
2021-02-11 19:08:46 +0000

    bla

---
+++ Game Engines 2 Examples 2021/Assets/Boid.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    List<SteeringBehaviour> behaviours = new List<SteeringBehaviour>();
+
+    public Vector3 force = Vector3.zero;
+    public Vector3 acceleration = Vector3.zero;
+    public Vector3 velocity = Vector3.zero;
+    public float mass = 1;
+
+    [Range(0.0f, 10.0f)]
+    public float damping = 0.01f;
+
+    [Range(0.0f, 1.0f)]
+    public float banking = 0.1f;
+    public float maxSpeed = 5.0f;
+    public float maxForce = 10.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        SteeringBehaviour[] behaviours = GetComponents<SteeringBehaviour>();
+
+        foreach (SteeringBehaviour b in behaviours)
+        {
+            this.behaviours.Add(b);            
+        }
+    }
+
+    public Vector3 SeekForce(Vector3 target)
+    {
+        Vector3 desired = target - transform.position;
+        desired.Normalize();
+        desired *= maxSpeed;
+        return desired - velocity;
+    }
+
+    public Vector3 ArriveForce(Vector3 target, float slowingDistance = 15.0f)
+    {
+        Vector3 toTarget = target - transform.position;
+
+        float distance = toTarget.magnitude;
+        if (distance < 0.1f)
+        {
+            return Vector3.zero;
+        }
+        float ramped = maxSpeed * (distance / slowingDistance);
+
+        float clamped = Mathf.Min(ramped, maxSpeed);
+        Vector3 desired = clamped * (toTarget / distance);
+
+        return desired - velocity;
+    }
+    
+
+    Vector3 Calculate()
+    {
+        force = Vector3.zero;
+
+        // Weighted prioritised truncated running sum
+        // 1. Behaviours are weighted
+        // 2. Behaviours are prioritised
+        // 3. Truncated
+        // 4. Running sum
+
+
+        foreach (SteeringBehaviour b in behaviours)
+        {
+            if (b.isActiveAndEnabled)
+            {
+                force += b.Calculate() * b.weight;
+
+                float f = force.magnitude;
+                if (f >= maxForce)
+                {
+                    force = Vector3.ClampMagnitude(force, maxForce);
+                    break;
+                }               
+            }
+        }
+
+        return force;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        force = Calculate();
+        Vector3 newAcceleration = force / mass;
+        acceleration = Vector3.Lerp(acceleration, newAcceleration, Time.deltaTime);
+        velocity += acceleration * Time.deltaTime;
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        
+        if (velocity.magnitude > float.Epsilon)
+        {
+            Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (acceleration * banking), Time.deltaTime * 3.0f);
+            transform.LookAt(transform.position + velocity, tempUp);
+
+            transform.position += velocity * Time.deltaTime;
+            velocity *= (1.0f - (damping * Time.deltaTime));
+        }
+    }
+}
