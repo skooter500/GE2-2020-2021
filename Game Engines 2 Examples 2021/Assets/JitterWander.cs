7f41c20d4cb4bb22f0fce56d680807a047e596b2
Bryan Duggan <skooter500@gmail.com>
2021-02-18 10:23:35 +0000

    wander starter

---
+++ Game Engines 2 Examples 2021/Assets/JitterWander.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JitterWander : SteeringBehaviour
+{
+
+    public void OnDrawGizmos()
+    {
+        
+
+    }
+
+
+    public override Vector3 Calculate()
+    {
+        return Vector3.Zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
