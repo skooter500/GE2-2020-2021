4e7c736c606ec89c8783dc07a0c7312739ebf344
Bryan Duggan <skooter500@gmail.com>
2021-02-26 10:02:15 +0000

    bla

--- Game Engines 2 Examples 2021/Assets/Harmonic.cs
+++ Game Engines 2 Examples 2021/Assets/Harmonic.cs
@@ -21,11 +21,38 @@ public class Harmonic : SteeringBehaviour
     // Start is called before the first frame update
     private void OnDrawGizmos()
     {
+        if (Application.isPlaying && isActiveAndEnabled)
+        {
+            Vector3 localCp = (Vector3.forward * distance);
+            Vector3 worldCP = transform.TransformPoint(localCp);
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(worldCP, radius);
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawLine(transform.position, worldTarget);
+        }
     }
 
     // Update is called once per frame
     public override Vector3 Calculate()
     {
-        return Vector3.zero;
+        float n  = Mathf.Sin(theta);
+        float angle = n * amplitude * Mathf.Deg2Rad;
+
+        Vector3 rot = transform.rotation.eulerAngles;
+
+        if (axis == Axis.Horizontal)
+        {
+            target.x = Mathf.Sin(angle);
+            target.z = Mathf.Cos(angle);
+            rot.z = 0;
+        }
+        target *= radius;
+
+        Vector3 localtarget = target + Vector3.forward * distance;
+        worldTarget = transform.position + Quaternion.Euler(rot) * localtarget;
+
+        theta += frequency * Time.deltaTime * Mathf.PI * 2.0f;
+
+        return boid.SeekForce(worldTarget);
     }
 }
