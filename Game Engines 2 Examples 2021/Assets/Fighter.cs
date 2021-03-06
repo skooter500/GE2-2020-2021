9da5fa72f06f44dffb9a588754eccf8057622aed
Bryan Duggan <skooter500@gmail.com>
2021-03-06 20:41:28 +0000

    text

--- Game Engines 2 Examples 2021/Assets/Fighter.cs
+++ Game Engines 2 Examples 2021/Assets/Fighter.cs
@@ -7,4 +7,15 @@ public class Fighter : MonoBehaviour
     public PlayerStats playerStats;
     public GameObject bullet;
     public GameObject enemy;
+    public TMPro.TextMeshPro text;
+
+    void Update()
+    {        
+        text.text = "Health: " + playerStats.health + "\n" + 
+            
+            "Ammo: " + playerStats.ammo + "\n" +
+            "State: " + GetComponent<StateMachine>().currentState.GetType().Name + "\n" +
+            "Global State: " + GetComponent<StateMachine>().globalState.GetType().Name;
+
+    }
 }
